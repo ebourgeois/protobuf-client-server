cf62928c0e0c0193012be732d032fb6a96005037
boureric <boureric@ms.com>
2014-09-22 19:28:45 -0400

    Initial client-server commit

---
+++ src/main/java/ca/jeb/ByteArrayToProtobufRequest.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2014 Erick Bourgeois, All Rights Reserved
+
+package ca.jeb.gpb;
+
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author <a href="mailto:erick@jeb.ca">Erick Bourgeois</a>
+ */
+public class ByteArrayToProtobufRequest implements Converter<S, T>
+{
+
+}
---
+++ src/main/java/ca/jeb/SimpleGateway.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2014 Erick Bourgeois, All Rights Reserved
+
+package ca.jeb.gpb;
+
+/**
+ * @author Erick Bourgeois
+ */
+public interface SimpleGateway
+{
+  String send(String text);
+}
